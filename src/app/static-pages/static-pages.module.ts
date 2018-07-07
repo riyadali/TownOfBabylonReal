aaed00adbacbe8e99e8c360f18764662286c46bd
riyadali <riyadali@gmail.com>
2018-07-07 15:51:18 -0400

    Update static-pages.module.ts

--- src/app/static-pages/static-pages.module.ts
+++ src/app/static-pages/static-pages.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { Page1Component } from './page1.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+ imports: [RouterModule.forChild([
+    { path: '', component: Page1Component }
+    
+  ])],
+  declarations: [Page1Component],
 })
 export class StaticPagesModule { }
