edf2c5ef28a6c96e100179faf670b41bc5a15815
riyadali <riyadali@gmail.com>
2018-07-28 14:00:54 -0400

    Update search-form.component.ts

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,1 +1,37 @@
+/* From web site https://itnext.io/using-angular-6-material-auto-complete-with-async-data-6d89501c4b79 */
+import { Component, OnInit } from '@angular/core';
+import {SearchUser, IUserResponse} from '../search-user.class';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {SearchService} from '../search.service';
+import {switchMap, debounceTime} from 'rxjs/operators';
+import {Observable} from 'rxjs'
 
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.scss']
+})
+export class SearchFormComponent implements OnInit {
+
+  filteredUsers: Observable<IUserResponse>;
+  usersForm: FormGroup;
+  
+  constructor(private fb: FormBuilder, private searchService: SearchService) {}
+
+  ngOnInit() {
+    this.usersForm = this.fb.group({
+      userInput: null
+    })
+
+    this.filteredUsers = this.usersForm.get('userInput').valueChanges
+      .pipe(
+        debounceTime(300),
+        switchMap(value => this.searchService.search({name: value}, 1))
+      );
+  }
+
+  displayFn(user: SearchUser) {
+    if (user) { return user.name; }
+  }
+
+}
