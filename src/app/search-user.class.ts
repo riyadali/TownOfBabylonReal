37b620607e716aa9dae2c8396e554e55ec93ddf1
riyadali <riyadali@gmail.com>
2018-08-21 13:30:23 -0400

    Update search-user.class.ts

--- src/app/search-user.class.ts
+++ src/app/search-user.class.ts
@@ -1,5 +1,3 @@
-/* This class or the interface is not used ... just updating the class for completeness sakes
-   I'm, now working directly with a array of JSON objects */
 export class SearchDoc {
   constructor(public label: string, public value: string) {}
 }
