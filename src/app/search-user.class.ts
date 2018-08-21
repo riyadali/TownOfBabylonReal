1c25b054af89b31bf89622bad2cfb1bd68174cfd
riyadali <riyadali@gmail.com>
2018-08-21 13:27:37 -0400

    Update search-user.class.ts

--- src/app/search-user.class.ts
+++ src/app/search-user.class.ts
@@ -1,5 +1,7 @@
-export class SearchUser {
-  constructor(public id: number, public name: string) {}
+/* This class or the interface is not used ... just updating the class for completeness sakes
+   I'm, now working directly with a array of JSON objects */
+export class SearchDoc {
+  constructor(public label: string, public value: string) {}
 }
 
 export interface IUserResponse {
