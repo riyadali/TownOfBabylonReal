a535aae5d89b20dae2fda11f2226f32cb035f569
riyadali <riyadali@gmail.com>
2018-08-22 13:26:07 -0400

    Update search.service.ts

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -18,7 +18,7 @@ export class SearchService {
     return this.http.get<IUserResponse>('/api/searchusers') */
     /* refer to this site on why pipe is needed https://www.academind.com/learn/javascript/rxjs-6-what-changed/ */
      let httpParams = new HttpParams().set('term', 'gar');
-     return this.http.get<SearchDoc[]>('https://www.townofbabylon.com/Search/AutoComplete', {httpParams})
+     return this.http.get<SearchDoc[]>('https://www.townofbabylon.com/Search/AutoComplete', {params: httpParams})
        .pipe(             
               /* code that is case sensitive
               map(docs => docs.filter(doc => doc.value.includes(filter.name))
